1f3dcf93a7faf37a5c983076ef29c4573f73fdf2
anderson <andersonnevesmaciel@gmail.com>
2025-05-26 19:01:44 -0300

    Initial commit of Flutter project

---
+++ lib/pages/treino_quadric_fpremium/treino_quadric_fpremium_model.dart
@@ -0,0 +1,13 @@
+import '/flutter_flow/flutter_flow_util.dart';
+import '/index.dart';
+import 'treino_quadric_fpremium_widget.dart' show TreinoQuadricFpremiumWidget;
+import 'package:flutter/material.dart';
+
+class TreinoQuadricFpremiumModel
+    extends FlutterFlowModel<TreinoQuadricFpremiumWidget> {
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
